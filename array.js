f6b498388d982af7020fdd0102e1b9c0dbef3e13
sindhia12 <Sindhiagaad@gmail.com>
2024-03-16 00:31:17 +0500

    first commit

---
+++ array.js
@@ -0,0 +1,13 @@
+var frndz = ['hina', 'sana', 1, 2, true];
+console.log(frndz);
+console.log(frndz[2]);
+var fruits = ['apple', "mango", "orange"];
+console.log(fruits);
+console.log(fruits[0]); // apple
+var feespaid = [true, false];
+console.log(feespaid);
+console.log(feespaid[1]);
+var ages = [11, 15, 20];
+console.log(ages);
+console.log(ages[2]); // 20
+console.log(fruits[6]); //undefined
---
+++ array.ts
@@ -0,0 +1,13 @@
+let frndz = ['hina', 'sana', 1, 2, true];
+console.log (frndz);
+console.log(frndz[2]);
+let fruits:string[]=['apple',"mango","orange"];
+console.log(fruits);
+console.log(fruits[0]); // apple
+let feespaid:boolean []=[true,false];
+console.log(feespaid);
+console.log(feespaid[1]);
+let ages:number[]=[11,15,20];
+console.log(ages);
+console.log(ages[2]);// 20
+console.log(fruits[6]);//undefined
